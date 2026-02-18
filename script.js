f34737badfa549912e75b26947000f1763596e08
AlexShtouff <alexkraft.new@gmail.com>
2026-02-18 11:17:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -178,12 +178,12 @@ function updateDirectionArrows() {
     });
 }
     
-//function handleOrientation(event) {
-  //  const now = Date.now();
-    //if (now - lastUpdate < 30) return; // Only update every 30ms
-    //lastUpdate = now;
+function handleOrientation(event) {
+    const now = Date.now();
+    if (now - lastUpdate < 30) return; // Only update every 30ms
+    lastUpdate = now;
 	
-	//if (event.alpha === null) return;
+	if (event.alpha === null) return;
 
     // alpha = compass heading (0 = north)
     //deviceHeading = event.alpha;
